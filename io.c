85fbda3b07e45331c7ff8231db7973ce3b17f86f
fionnmohan01 <fionnmohan01@gmail.com>
2026-05-04 20:30:04 +0100

    written loadData function, included waveform.h in io.h. and edited error returns as return 1 would have interfered with the program

--- io.c
+++ io.c
@@ -3,14 +3,17 @@
 // Requirement
 
 #include "io.h"
-
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "waveform.h"
 
 int countRows(char *fileName){
     FILE *file = fopen(fileName, "r");
         if (file == NULL) {
             printf("Error opening file. \n");
-            return 1;
+            return NULL;
         }
     int totalCount = 0;
     char line[256];
@@ -23,3 +26,56 @@ int countRows(char *fileName){
     fclose(file);
     return totalCount;
 }
+
+waveformSample *loadData(char *fileName, int totalCount) {
+    FILE *file = fopen(fileName, "r");
+    if (file == NULL) {
+        printf("Error opening file. \n");
+        return NULL;
+    }
+        //allocate memory
+    waveformSample *data = malloc(totalCount * sizeof(waveformSample));
+    if (data == NULL) {
+        printf("Error allocating memory. \n");
+        fclose(file);
+        return NULL;
+    }
+    char line[256];
+        //skip header
+    fgets(line,sizeof(line),file);
+
+    for (int i = 0; i < totalCount; i++) {
+        fgets(line,sizeof(line), file);
+
+        char *token;
+
+        token = strtok(line, ",");
+        data[i].timestamp = atof(token);
+
+        token = strtok(NULL, ",");
+        data[i].phase_A_voltage = atof(token);
+
+        token = strtok(NULL, ",");
+        data[i].phase_B_voltage = atof(token);
+
+        token = strtok(NULL, ",");
+        data[i].phase_C_voltage = atof(token);
+
+        token = strtok(NULL, ",");
+        data[i].line_current = atof(token);
+
+        token = strtok(NULL, ",");
+        data[i].frequency = atof(token);
+
+        token = strtok(NULL, ",");
+        data[i].power_factor = atof(token);
+
+        token = strtok(NULL, ",");
+        data[i].thd_percent = atof(token);
+    }
+
+    fclose(file);
+    return data;
+}
+
+
--- io.h
+++ io.h
@@ -1,14 +1,11 @@
 //
 // Created by 24029135 on 08/04/2026.
 //Requirements
-
 #ifndef IO_H
 #define IO_H
+#include "waveform.h"
 //Functions
 int countRows(char *fileName);
-double loadData(char *fileName, int totalCount);
-
-
-
+waveformSample *loadData(char *fileName, int totalCount);
 
 #endif //IO_H
